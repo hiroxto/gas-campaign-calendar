9d74e07555c26f9e84b90749510f5f5f9ac64f84
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-02-27 19:21:31 +0900

    Add typescript-eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
   },
   env: {
     'googleappsscript/googleappsscript': true,
   },
   extends: [
+    'plugin:@typescript-eslint/recommended',
     'plugin:promise/recommended',
     'standard',
   ],
   plugins: [
+    '@typescript-eslint',
     'promise',
     'googleappsscript',
   ],
@@ -22,5 +24,8 @@ module.exports = {
     indent: ['error', 2],
     'quote-props': ['error', 'as-needed'],
     'promise/catch-or-return': ['error', { allowThen: true, terminationMethod: ['catch', 'asCallback', 'finally'] }],
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/no-use-before-define': 'off',
   },
 };
